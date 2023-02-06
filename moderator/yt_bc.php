50c5deeb915cd260b4b62d54f310ad48b8d1dff0
PHPVibe <office@phpvibe.com>
2023-02-06 23:29:34 +0200

    quickfixes to channel import

--- moderator/yt_bc.php
+++ moderator/yt_bc.php
@@ -1,5 +1,6 @@
 <?php  $the = $_GET;
 if(!isset($the['auto'])) { $the['auto'] = 0;}
+if(!isset($the['categ'])) { $the['categ'] = 1;}
 if(!isset($the['imode']) || nullval($the['imode'])) { $the['imode'] = 2;}
 $youtube = new Youtube(array('key' => get_option('youtubekey')));
 if(isset($the['c'])) {
@@ -22,21 +23,24 @@ if(isset($channel) && $channel) {
 		if(isset($channel->contentDetails->relatedPlaylists->uploads)) {
 		$own[] = $youtube->getPlaylistById($channel->contentDetails->relatedPlaylists->uploads);
 	    }
-	if(!nullval($own)) {
-echo '<ol class="ytChannel row">';
-foreach ($own as $video) {
-echo '<li class="holder">';
-echo '<div class="YtScene"><div class="cover"><img src="'.$video->snippet->thumbnails->medium->url.'" style="width:100%; height:238px;"/></div>
-<div class="bubble"><a href="'.admin_url('yt_playlistsearch').'&pID='.$video->id.'&categ='.$the['categ'].'&owner='.$the['owner'].'&auto='.$the['auto'].'&imode='.$the['imode'].'" class="tipS" title="Explore playlist & import options in new tab" target="_blank"><i class="icon-list"></i></a></div>
-</div>';
-echo '<h2>'.$video->snippet->title.'</h2>';
-echo '<div class="yfooter">  by <a href="https://www.youtube.com/channel/'.$video->snippet->channelId.'" class="tipS" title="See this channel on Youtube" target="_blank"><i class="icon-youtube"></i> '.$video->snippet->channelTitle.' </a>
-<div class="import"><a href="https://www.youtube.com/playlist?list='.$video->id.'" class="tipS" title="Explore this playlist on Youtube" target="_blank"><i class="icon-youtube"></i> </a></div></div>
-';
-echo '</li>';	
-}
-echo '</ol>';
-}	
+		if(!nullval($own)) {
+		echo '<ol class="ytChannel row">';
+				foreach ($own as $video) {
+					
+					if(isset($video->id)) {
+						echo '<li class="holder">';
+						echo '<div class="YtScene"><div class="cover"><img src="'.$video->snippet->thumbnails->medium->url.'" style="width:100%; height:238px;"/></div>
+						<div class="bubble"><a href="'.admin_url('yt_playlistsearch').'&pID='.$video->id.'&categ='.$the['categ'].'&owner='.$the['owner'].'&auto='.$the['auto'].'&imode='.$the['imode'].'" class="tipS" title="Explore playlist & import options in new tab" target="_blank"><i class="icon-list"></i></a></div>
+						</div>';
+						echo '<h2>'.$video->snippet->channelTitle.'</h2>';
+						echo '<div class="yfooter">  by <a href="https://www.youtube.com/channel/'.$video->snippet->channelId.'" class="tipS" title="See this channel on Youtube" target="_blank"><i class="icon-youtube"></i> '.$video->snippet->channelTitle.' </a>
+						<div class="import"><a href="https://www.youtube.com/playlist?list='.$video->id.'" class="tipS" title="Explore this playlist on Youtube" target="_blank"><i class="icon-youtube"></i> </a></div></div>
+						';
+						echo '</li>';
+					}
+				}
+		echo '</ol>';
+		}	
 		
 	}
 echo '<h1> Other Playlists</h1>';	
@@ -66,8 +70,3 @@ break;
 } else {
 echo "Wrong name or id";	
 }
-
-
-
-
-
