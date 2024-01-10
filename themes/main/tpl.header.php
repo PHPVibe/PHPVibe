d40363f50708590c7987e7a941bca2c8cde80861
PHPVibe <office@phpvibe.com>
2024-01-10 14:25:18 +0200

    Update tpl.header.php

--- themes/main/tpl.header.php
+++ themes/main/tpl.header.php
@@ -5,7 +5,7 @@ if(!is_home()) {
 
 /* register_style('roboto'); */
 if(_get('darkmode')) {
-    $_SESSION['darkmode'] = _get_int('darkmode');
+    $_SESSION['darkmode'] = _get('darkmode');
 }
 if(isset( $_SESSION['darkmode']) && ( $_SESSION['darkmode'] == 1) ) {
     register_style('dark');
@@ -190,10 +190,11 @@ function top_nav(){
 	<a href="javascript:showLogin()" class="btn btn-primary btn-small btn-block">'._lang("Join").'</a>
 	';
 	}
-    if(_contains(canonical(), '?')) { $darklink = canonical().'&darkmode='; } else {$darklink = canonical().'?darkmode=';}
+    //if(_contains(canonical(), '?')) { $darklink = canonical().'&darkmode='; } else {$darklink = canonical().'?darkmode=';}
+    $darklink = canonical().'?darkmode=';
     if(isset($_SESSION['darkmode']) &&  ($_SESSION['darkmode']== 1)) {
 
-        $nav .= '<a href="'.$darklink.'0" class="top-link">
+        $nav .= '<a href="'.$darklink.'2" class="top-link">
 <svg id="theme-toggle-light-icon" class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10 15a5 5 0 1 0 0-10 5 5 0 0 0 0 10Zm0-11a1 1 0 0 0 1-1V1a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1Zm0 12a1 1 0 0 0-1 1v2a1 1 0 1 0 2 0v-2a1 1 0 0 0-1-1ZM4.343 5.757a1 1 0 0 0 1.414-1.414L4.343 2.929a1 1 0 0 0-1.414 1.414l1.414 1.414Zm11.314 8.486a1 1 0 0 0-1.414 1.414l1.414 1.414a1 1 0 0 0 1.414-1.414l-1.414-1.414ZM4 10a1 1 0 0 0-1-1H1a1 1 0 0 0 0 2h2a1 1 0 0 0 1-1Zm15-1h-2a1 1 0 1 0 0 2h2a1 1 0 0 0 0-2ZM4.343 14.243l-1.414 1.414a1 1 0 1 0 1.414 1.414l1.414-1.414a1 1 0 0 0-1.414-1.414ZM14.95 6.05a1 1 0 0 0 .707-.293l1.414-1.414a1 1 0 1 0-1.414-1.414l-1.414 1.414a1 1 0 0 0 .707 1.707Z"></path>
         </svg>
