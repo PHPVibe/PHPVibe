7ac55017682714c63ae3f9e50e7a8f7bfbb17e75
PHPVibe <office@phpvibe.com>
2024-01-19 14:55:38 +0200

    Update plugin.php

--- plugins/izlesene/plugin.php
+++ plugins/izlesene/plugin.php
@@ -3,33 +3,41 @@
  * Plugin Name: Izlesene.com source
  * Plugin URI: http://phpvibe.com/
  * Description: Adds Izlesene embed source to PHPVibe
- * Version: 2.0
+ * Version: 3.0
  * Author: PHPVibe Crew
  * Author URI: http://www.phpvibe.com
  * License: Commercial
  */
-function _Izlesene($hosts = array()){
-$hosts[] = 'izlesene';
-return $hosts;
+function _Izlesene($hosts = array())
+{
+    $hosts[] = 'izlesene';
+    return $hosts;
 }
-function EmbedIzlesene($txt =''){
-global $vid;
-if(isset($vid)) {
-if($vid->VideoProvider($vid->theLink()) == 'izlesene' ) {	
-$link = $vid->theLink();if(!nullval($link)) {	
-$variable = $vid->theLink();$variable  = substr($variable, 0, strpos($variable, "#list"));$id = $vid->getLastNr($variable);if(!nullval($id)) {
-$tembed = ' <iframe src="https://www.izlesene.com/embedplayer/'.$id.'/?showrel=0&loop=0&autoplay=1&autohide=1&showinfo=1&socialbuttons=0" allowfullscreen="true" frameborder="0"></iframe>';
-$txt .= $tembed;
+
+function EmbedIzlesene($txt = '')
+{
+    global $VibeProvider, $VibeLink;
+    if (isset($VibeProvider) && isset($VibeLink)) {
+
+        if (_contains(strtolower($VibeProvider), 'izlesene')) {
+            if (!nullval($VibeLink)) {
+                $variable = $VibeLink;
+                $variable = substr($variable, 0, strpos($variable, "#list"));
+                $id = $vid->getLastNr($variable);
+                if (!nullval($id)) {
+                    $tembed = ' <iframe src="https://www.izlesene.com/embedplayer/' . $id . '/?showrel=0&loop=0&autoplay=1&autohide=1&showinfo=1&socialbuttons=0" allowfullscreen="true" frameborder="0"></iframe>';
+                    $txt .= $tembed;
+                }
+                /* End link check */
+            }
+            /* End provider check */
+        }
+        /* End isset(vid) */
+    }
+    /* End function */
+    return $txt;
 }
-/* End link check */
-}
-/* End provider check */
-}
-/* End isset(vid) */
-}
-/* End function */
-return $txt;
-}
+
 add_filter('EmbedModify', 'EmbedIzlesene');
 add_filter('vibe-video-sources', '_Izlesene');
 ?>
