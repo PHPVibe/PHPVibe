3314d85a43c0166e64cd6a3b1c27b7df0276518f
PHPVibe <office@phpvibe.com>
2024-01-19 15:01:32 +0200

    Update plugin.php

--- plugins/izlesene/plugin.php
+++ plugins/izlesene/plugin.php
@@ -6,9 +6,19 @@
  * Version: 3.0
  * Author: PHPVibe Crew
  * Author URI: http://www.phpvibe.com
- * License: Commercial
- */
-function _Izlesene($hosts = array())
+ * License: GPL
+ 
+function getIzleseneID($url)
+{
+    $pieces_array = explode('/', $url);
+    $end_piece = end($pieces_array);
+    $id_pieces = explode('-', $end_piece);
+    $last_piece = end($id_pieces);
+    $videoId = preg_replace("/[^0-9]/", "", $last_piece);
+    return $videoId;
+}
+
+function RegisterIzlesene($hosts = array())
 {
     $hosts[] = 'izlesene';
     return $hosts;
@@ -23,7 +33,7 @@ function EmbedIzlesene($txt = '')
             if (!nullval($VibeLink)) {
                 $variable = $VibeLink;
                 $variable = substr($variable, 0, strpos($variable, "#list"));
-                $id = $vid->getLastNr($variable);
+                $id = getIzleseneID($variable);
                 if (!nullval($id)) {
                     $tembed = ' <iframe src="https://www.izlesene.com/embedplayer/' . $id . '/?showrel=0&loop=0&autoplay=1&autohide=1&showinfo=1&socialbuttons=0" allowfullscreen="true" frameborder="0"></iframe>';
                     $txt .= $tembed;
@@ -39,5 +49,5 @@ function EmbedIzlesene($txt = '')
 }
 
 add_filter('EmbedModify', 'EmbedIzlesene');
-add_filter('vibe-video-sources', '_Izlesene');
+add_filter('vibe-video-sources', 'RegisterIzlesene');
 ?>
