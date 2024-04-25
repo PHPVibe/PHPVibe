4bfa0e589fa91ac4e18fb41e74efa01aa61ea462
PHPVibe <office@phpvibe.com>
2024-04-25 15:45:16 +0300

    Update pass.php

--- pass.php
+++ pass.php
@@ -1,2 +1,5 @@
-<?php require_once("load.php");
-user::LoginUser(2);
+<?php 
+//Example login by code
+require_once("load.php");
+//user::LoginUser(12);
+//Uncomment and replace the user id with your desired user id.
